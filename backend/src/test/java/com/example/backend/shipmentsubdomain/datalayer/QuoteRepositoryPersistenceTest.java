1de1bc71f2eb398dd5c9fa1d3434885671b36d6a
Richard Danon-Woedey <richard2004danon@gmail.com>
2023-12-05 11:33:53 -0500

    Tests 37%

--- backend/src/test/java/com/example/backend/shipmentsubdomain/datalayer/QuoteRepositoryPersistenceTest.java
+++ backend/src/test/java/com/example/backend/shipmentsubdomain/datalayer/QuoteRepositoryPersistenceTest.java
@@ -1,7 +1,50 @@
 package com.example.backend.shipmentsubdomain.datalayer;
 
+import com.example.backend.shipmentsubdomain.presentationlayer.QuoteResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
 class QuoteRepositoryPersistenceTest {
+
+    @Autowired
+    private QuoteRepository quoteRepository;
+
+    @BeforeEach
+    public void setUp() {
+
+    }
+
+    @Test
+    public void whenSaveQuoteWithValues_thenValuesAreCorrectlyPersisted() {
+        // Arrange: Create a sample Quote
+        Quote quote = new Quote(
+                new PickupAddress("123 Main St", "CityA", "ProvinceA", Country.CA, "12345", 101, true, "Apartment"),
+                new DestinationAddress("456 Oak St", "CityB", "ProvinceB", Country.USA, "54321", 202, false, "House"),
+                new ContactDetails("John", "Doe", "john.doe@example.com", "123-456-7890"),
+                ContactMethod.EMAIL,
+                LocalDate.of(2023, 1, 1),
+                "Additional comments go here"
+        );
+
+        // Act
+        Quote savedQuote = quoteRepository.save(quote);
+
+        // Asser
+        assertThat(savedQuote.getExpectedMovingDate()).isEqualTo(LocalDate.of(2023, 1, 1));
+        assertThat(savedQuote.getPickupAddress().getPickupProvince()).isEqualTo("ProvinceA");
+        assertThat(savedQuote.getContactMethod()).isEqualTo(ContactMethod.EMAIL);
+        assertThat(savedQuote.getComment()).isEqualTo("Additional comments go here");
+
+    }
+
 
 }
--- backend/src/test/java/com/example/backend/shipmentsubdomain/presentationlayer/QuoteControllerIntegrationTest.java
+++ backend/src/test/java/com/example/backend/shipmentsubdomain/presentationlayer/QuoteControllerIntegrationTest.java
@@ -1,10 +1,21 @@
 package com.example.backend.shipmentsubdomain.presentationlayer;
 
+import com.example.backend.shipmentsubdomain.datalayer.ContactMethod;
+import com.example.backend.shipmentsubdomain.datalayer.Country;
+import com.example.backend.shipmentsubdomain.datalayer.QuoteRepository;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.time.LocalDate;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
@@ -14,7 +25,59 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @AutoConfigureMockMvc(addFilters = false)
 @ActiveProfiles("test")
 class QuoteControllerIntegrationTest {
-    private final String BASE_URI_GALLERIES = "/api/v1/movingexpress/quotes/request";
+    private final String BASE_URI_QUOTES = "/api/v1/movingexpress/quotes/request";
 
+    @Autowired
+    WebTestClient webTestClient;
 
+    @Autowired
+    QuoteRepository quoteRepository;
+
+    @BeforeEach
+    public void setUp(){
+
+    }
+
+    @Test
+    public void whenAddQuoteWithValidValues_thenReturnNewQuote(){
+        //arrange
+        QuoteRequest quoteRequest = QuoteRequest.builder()
+                .pickupStreetAddress("123 Main St")
+                .pickupCity("CityA")
+                .pickupProvince("ProvinceA")
+                .pickupCountry(Country.CA)
+                .pickupPostalCode("12345")
+                .pickupRoomNumber(101)
+                .pickupElevator(true)
+                .pickupBuildingType("Apartment")
+                .destinationStreetAddress("456 Oak St")
+                .destinationCity("CityB")
+                .destinationProvince("ProvinceB")
+                .destinationCountry(Country.USA)
+                .destinationPostalCode("54321")
+                .destinationRoomNumber(202)
+                .destinationElevator(false)
+                .destinationBuildingType("House")
+                .firstName("John")
+                .lastName("Doe")
+                .emailAddress("john.doe@example.com")
+                .phoneNumber("123-456-7890")
+                .expectedMovingDate(LocalDate.of(2023, 1, 1))
+                .contactMethod(ContactMethod.EMAIL)
+                .comment("Additional comments go here")
+                .build();
+
+        //act and assert
+        webTestClient.post()
+                .uri(BASE_URI_QUOTES)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(quoteRequest)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.quoteId").isNotEmpty()
+                .jsonPath("$.pickupProvince").isEqualTo(quoteRequest.getPickupProvince());
+    }
 }
