7f9b1de5a2a62ee7ca40f2e5937caf9145be4c68
Richard Danon-Woedey <richard2004danon@gmail.com>
2023-12-05 03:32:55 -0500

    preperation

---
+++ backend/src/test/java/com/example/backend/shipmentsubdomain/presentationlayer/QuoteControllerIntegrationTest.java
@@ -0,0 +1,20 @@
+package com.example.backend.shipmentsubdomain.presentationlayer;
+
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@AutoConfigureWebClient
+@AutoConfigureMockMvc(addFilters = false)
+@ActiveProfiles("test")
+class QuoteControllerIntegrationTest {
+    private final String BASE_URI_GALLERIES = "/api/v1/movingexpress/quotes/request";
+
+
+}
