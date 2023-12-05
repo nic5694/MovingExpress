516bf61e12c556b4774f6cad2ed48c744814fcda
nic5694 <nicholasmartoccia04@icloud.com>
2023-12-05 17:38:24 -0500

    removed auth0 spa

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,19 +3,10 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { Auth0Provider } from '@auth0/auth0-react'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-    <Auth0Provider
-        domain="movingexpress.us.auth0.com"
-        clientId="RlhU3I8p4TqF9Qygmiemc8AW3yFBeoa2"
-        authorizationParams={{
-            redirect_uri: window.location.origin,
-        }}
-    >
         <App />
-    </Auth0Provider>
 )
 
 // If you want to start measuring performance in your app, pass a function
