8f486c338d7ca1bc4a9740dd059dbeac0ef47688
Caleb Liu <liucaleb8@gmail.com>
2023-12-17 03:54:56 -0500

    Implemented frontend for quote details, started frontend test

--- backend/src/main/java/com/example/backend/shipmentsubdomain/businesslayer/QuoteServiceImpl.java
+++ backend/src/main/java/com/example/backend/shipmentsubdomain/businesslayer/QuoteServiceImpl.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.List;
 
+
 @Service
 @RequiredArgsConstructor
 public class QuoteServiceImpl implements QuoteService {
